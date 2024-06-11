d8e28a40ce29cc8a60282bbfc142a4ecd1a118fa
JuanPescoran <u20221c936@upc.edu.pe>
2024-06-11 03:14:24 -0500

    feat: added configures for database and dependency injections for bounded context

---  si730pc2u20221c936.API/Program.cs
+++  si730pc2u20221c936.API/Program.cs
@@ -1,17 +1,69 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using si730pc2u20221c936.API.Shared.Domain.Repositories;
+using si730pc2u20221c936.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using si730pc2u20221c936.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using si730pc2u20221c936.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using si730pc2u20221c936.API.Subscriptions.Application.Internal.CommandServices;
+using si730pc2u20221c936.API.Subscriptions.Domain.Repositories;
+using si730pc2u20221c936.API.Subscriptions.Domain.Services;
+using si730pc2u20221c936.API.Subscriptions.Infrastructure.Persistence.EFC.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add Configuration for Routing
+
+builder.Services.AddControllers( options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
+// Configure Lowercase URLs
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Configure Database Context and Logging Levels
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    if (connectionString == null) return;
+    if (builder.Environment.IsDevelopment()) 
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
+    else if (builder.Environment.IsProduction()) 
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Error)
+            .EnableDetailedErrors();
+}); 
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure Dependency Injection
+
+// Shared Bounded Context Injection Configuration
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Subscriptions Bounded Context Injection Configuration
+builder.Services.AddScoped<IPlanRepository, PlanRepository>();
+builder.Services.AddScoped<IPlanCommandService, PlanCommandService>();
+
+
 var app = builder.Build();
+
+// Verify Database Objects are Created
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---  si730pc2u20221c936.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++  si730pc2u20221c936.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,11 +1,13 @@
 ﻿using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
 using si730pc2u20221c936.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using si730pc2u20221c936.API.Subscriptions.Domain.Model.Aggregates;
 
 namespace si730pc2u20221c936.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
+    public DbSet<Plan> Plans { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
         base.OnConfiguring(builder);
@@ -18,7 +20,11 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
         base.OnModelCreating(builder);
         
         // Planning Context
-        
+        builder.Entity<Plan>().HasKey(p => p.Id);
+        builder.Entity<Plan>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Plan>().Property(p => p.Name).IsRequired().HasMaxLength(30);
+        builder.Entity<Plan>().Property(p => p.MaxUsers).IsRequired();
+        builder.Entity<Plan>().Property(p => p.IsDefault).IsRequired();
         
         // Apply SnakeCase Naming Convention
         builder.UseSnakeCaseWithPluralizedTableNamingConvention();
