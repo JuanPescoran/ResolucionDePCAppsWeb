b6606e018d4a24f037d1dfacf2a1afe959c09b11
JuanPescoran <u20221c936@upc.edu.pe>
2024-06-11 03:15:13 -0500

    feat(subscription): created plan aggregate

---
+++  si730pc2u20221c936.API/Subscriptions/Domain/Model/Aggregates/Plan.cs
@@ -0,0 +1,25 @@
+﻿using si730pc2u20221c936.API.Subscriptions.Domain.Model.Commands;
+
+namespace si730pc2u20221c936.API.Subscriptions.Domain.Model.Aggregates;
+
+public class Plan
+{
+    public int Id { get;}
+    public string Name { get; private set; }
+    public int MaxUsers { get; private set; }
+    public int IsDefault { get; private set; }
+    
+    public Plan(string name, int maxUsers, int isDefault)
+    {
+        Name = name;
+        MaxUsers = maxUsers;
+        IsDefault = isDefault;
+    }
+
+    public Plan(CreatePlanCommand command)
+    {
+        Name = command.Name;
+        MaxUsers = command.MaxUsers;
+        IsDefault = command.IsDefault;
+    }
+}
---
+++  si730pc2u20221c936.API/Subscriptions/Domain/Model/Commands/CreatePlanCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace si730pc2u20221c936.API.Subscriptions.Domain.Model.Commands;
+
+public record CreatePlanCommand(string Name, int MaxUsers, int IsDefault);
