85816d9ab9771d953bb14fa8ffcf036913437671
JuanPescoran <u20221c936@upc.edu.pe>
2024-06-11 03:17:25 -0500

    feat(subscription): added interfaces for command service and repository

---
+++  si730pc2u20221c936.API/Subscriptions/Domain/Repositories/IPlanRepository.cs
@@ -0,0 +1,9 @@
+﻿using si730pc2u20221c936.API.Shared.Domain.Repositories;
+using si730pc2u20221c936.API.Subscriptions.Domain.Model.Aggregates;
+
+namespace si730pc2u20221c936.API.Subscriptions.Domain.Repositories;
+
+public interface IPlanRepository : IBaseRepository<Plan>
+{
+    Task<bool> DoesPlanExistWithName(string name);
+}
---
+++  si730pc2u20221c936.API/Subscriptions/Domain/Services/IPlanCommandService.cs
@@ -0,0 +1,10 @@
+﻿using si730pc2u20221c936.API.Subscriptions.Domain.Model.Aggregates;
+using si730pc2u20221c936.API.Subscriptions.Domain.Model.Commands;
+
+namespace si730pc2u20221c936.API.Subscriptions.Domain.Services;
+
+public interface IPlanCommandService
+{
+    Task<bool> DoesPlanExistWithName(string name);
+    Task<Plan?> Handle(CreatePlanCommand command);
+}
