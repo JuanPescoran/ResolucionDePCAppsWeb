97996a71dc06848057339f8bc05ddec277952b7c
JuanPescoran <u20221c936@upc.edu.pe>
2024-06-11 03:15:37 -0500

    feat(subscription): added plan repository

---
+++  si730pc2u20221c936.API/Subscriptions/Infrastructure/Persistence/EFC/Repositories/PlanRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using si730pc2u20221c936.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using si730pc2u20221c936.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using si730pc2u20221c936.API.Subscriptions.Domain.Model.Aggregates;
+using si730pc2u20221c936.API.Subscriptions.Domain.Repositories;
+
+namespace si730pc2u20221c936.API.Subscriptions.Infrastructure.Persistence.EFC.Repositories;
+
+public class PlanRepository(AppDbContext context) : BaseRepository<Plan>(context), IPlanRepository
+{
+    public async Task<bool> DoesPlanExistWithName(string name)
+    {
+        return await Context.Plans.AnyAsync(p => p.Name == name);
+    }
+}
